ac89a57db430d1e93e1f362122d68216b4025025
katherine <katherinesofia719@gmail.com>
2024-04-07 18:16:12 -0600

    Primer commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+// Declarar variables 
+const mensajeInput = document.getElementById("mensaje");
+const respuestaDiv = document.getElementById("respuesta");
+
+
+function mostrarMensaje() {
+  const mensaje = mensajeInput.value;
+
+  if (mensaje === "Si") {
+    respuestaDiv.textContent = "I love you";
+  }
+  else if (mensaje === "No"){
+    respuestaDiv.textContent = "Fatal Error crying";
+    
+  } else {
+    respuestaDiv.textContent = `Debes de elegir una opcion valida xd`;
+  }
+}
+
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  mostrarMensaje();
+});
